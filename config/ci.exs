98da47e664b417fe1669a7b27fe3bc91d4913d90
udit <yudistrange@gmail.com>
2019-05-17 12:04:07 +0530

    Configure travis-ci to setup test DB

---
+++ config/ci.exs
@@ -0,0 +1,18 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :shikche, ShikcheWeb.Endpoint,
+  http: [port: 4002],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :shikche, Shikche.Repo,
+  username: "postgres",
+  password: "",
+  database: "shikche_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
