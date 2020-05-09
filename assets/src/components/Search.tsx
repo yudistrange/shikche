9d3169f4efa6d063f6e187b630dd6de36eb5d2c2
udit <yudistrange@gmail.com>
2020-05-10 00:05:36 +0530

    Fix: Remove the word translation table when there are no words

--- assets/src/components/Search.tsx
+++ assets/src/components/Search.tsx
@@ -35,7 +35,7 @@ export class Search extends React.Component<{}, SearchState> {
           <input className="column column-10 column-offset-10" type="button" onClick={(e) => this.handleSearch()} value="Search" />
           <br />
         </div>
-        {this.state.result.translation !== '' && this.state.result.word !== '' &&
+        {this.state.result.translation && this.state.result.word &&
           <Word translation={this.state.result.translation} word={this.state.result.word} />
         }
       </>
