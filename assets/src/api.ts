145d3ebe6e5818c8d6346225a028d01eb5a0875f
udit <yudistrange@gmail.com>
2020-05-14 19:20:23 +0530

    Add API for login

--- assets/src/api.ts
+++ assets/src/api.ts
@@ -2,4 +2,17 @@ function searchBackend(value: string) {
   return fetch("/api/v1/translations/" + value)
 }
 
-export {searchBackend}
+function login(email: string, password: string) {
+  return fetch(
+    "/api/v1/login",
+    {
+      method: "post",
+      headers: {"Content-type": "application/json"},
+      body: JSON.stringify({
+        "email": email,
+        "password": password
+      })
+  })
+}
+
+export {searchBackend, login}
