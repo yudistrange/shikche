6b87ecc7d14207e4f9a40ec0a85c8fd72220013f
udit <yudistrange@gmail.com>
2020-05-16 19:15:36 +0530

    Add a message about translation failure

--- assets/src/components/Search.tsx
+++ assets/src/components/Search.tsx
@@ -3,29 +3,42 @@ import {Word} from './Word';
 import {searchBackend} from '../api';
 import './Search.css'
 
+const SearchFailed = () => (
+  <div className="search-failure-message">
+    Translation doesn't exist
+  </div>
+)
+
 interface SearchState {
   text: string,
-  result: { translation: string, word: string },
+  result: { translation: string, word: string, example: string},
+  searchFailed?: boolean,
 }
 
 export class Search extends React.Component<{}, SearchState> {
   state = {
     text: '',
-    result: {translation: '', word: ''},
+    result: {translation: '', word: '', example: ''},
+    searchFailed: false,
   }
 
   handleSearchInput(e: React.ChangeEvent<HTMLInputElement>) {
     const text = e.target.value;
-    this.setState({ text: text });
+    this.setState({ text: text, searchFailed: false });
   }
 
   async handleSearch() {
-    const result = await searchBackend(this.state.text).then((res) => res.json());
-    this.setState((state, _props) => {
-      return {
-        text: state.text, result: result
-      }
-    })
+    const response = await searchBackend(this.state.text)
+    if (!response.ok) {
+      this.setState({searchFailed: true})
+    } else {
+      const result = await response.json()
+      this.setState((state, _props) => {
+        return {
+          text: state.text, result: result,
+        }
+      })
+    }
   }
 
   render() {
@@ -38,6 +51,7 @@ export class Search extends React.Component<{}, SearchState> {
         {this.state.result.translation && this.state.result.word &&
           <Word translation={this.state.result.translation} word={this.state.result.word} />
         }
+        {this.state.searchFailed === true && <SearchFailed />}
       </>
     )
   }
